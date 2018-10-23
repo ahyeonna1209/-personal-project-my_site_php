68283376971bfe76e5d42a5d9e0e7b2d29459b03
ahyeonna1209 <36820261+ahyeonna1209@users.noreply.github.com>
2018-10-23 15:08:32 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+       
+        <title></title>
+    </head>
+    <body>
+        <!--no/id/user_id/name/pw/memo/rdgdate/ip -->
+        <form action="./test2.php" name="test" method="post">
+           <input type='hidden' name="id" value="test">
+           <li>id : <input type="text" name="user_id" size="10">
+           <li>name : <input type="text" name="name" size="10">
+           <li>password : <input type="password" name="pw" size="10">
+           <li>memo : <br/>
+           <textarea name="memo"></textarea>
+           <input type="submit" value="send">
+           
+           <?
+               $connect = mysql_connect('localhost','root','');
+               mysql_select_db('local',$connect);
+               
+               if(!$connect)die("can't connect to mySql".mysql_error());
+               
+               //calling data from query
+               $query = "select * from bbs_1 where id='id'";
+               $result = mysql_query($query, $connect);
+               while($data = mysql_fetch_array($result)){
+                   
+                   $date_Y=substr($data[regdate],0,4);
+                   $date_m=substr($data[regdate,4,2]);
+                   $date_d=substr($data[regdate,6,2]);
+                   $date_h = substr($data[regdate],8,2);
+                   $date_i = substr($data[regdate],10,2);
+                   
+                    
+                   -name : <?php echo $data[user_id]; ?>
+                   -id : <? echo $data[id]; ?>
+                   -memo : <? echo $data[memo]; ?>
+                   -date : <?= $data_Y ?>.<?= $data_m ?>.<?= $data_d?>
+                   
+               }
+            ?>
+        </form>
+    </body>
+</html>
