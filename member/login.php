5ab29d88df71eb9aeb64f6fc1de78ec9b461098e
ahyeonna1209 <36820261+ahyeonna1209@users.noreply.github.com>
2018-10-26 14:42:10 +1300

    create 'LogIn' page

---
+++ member/login.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="type/html" />
+        <title>Login Page</title>
+    </head>
+    
+    <body>
+        Log in
+        
+        <form action="./login_post.php" name="login" method="post">
+            <li>Id : <input type="text" name="user_id" size="10"/></li>
+            <li>Password : <input type="password" name="pw" size="15" /></li>
+        
+            <input type="submit" value="submit"/>
+        </form>
+        
+    </body>
+</html>
