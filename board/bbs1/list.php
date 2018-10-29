54d72b43fe71e1b8bef5142360361e1e57c7e373
ahyeonna1209 <36820261+ahyeonna1209@users.noreply.github.com>
2018-10-29 13:35:21 +1300

    Create list.php

---
+++ board/bbs1/list.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html" />
+        <link type="text/css" href="../lib/m_style.css" rel="stylesheet" />
+        <title>TEST BOARD</title>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
