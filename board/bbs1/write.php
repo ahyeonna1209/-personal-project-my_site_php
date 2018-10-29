0333621d760b7c2cd5581e143a4c888a37d13e37
ahyeonna1209 <36820261+ahyeonna1209@users.noreply.github.com>
2018-10-29 13:36:04 +1300

    Add files via upload

---
+++ board/bbs1/write.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html;" />
+        <link type="text/css" href="../lib/m_styole.css" rel="stylesheet" />
+        <title>Writing post in the board</title>
+    </head>
+    
+    <body>
+      
+      no
+      id
+      level
+      user_id
+      name
+      nick_name
+      email
+      subject
+      story
+      regdate
+      ip  
+    </body>
+</html>
---
+++ board/bbs1/write_post.php
@@ -0,0 +1,31 @@
+<?header("content-type:text/html; charset=UTF-8");
+
+    $id=$_POST[id];
+    $user_id=$_POST[user_id];
+    $name=$_POST[name];
+    $pw=$_POST[pw];
+    $memo=$_POST[memo];
+    $regdate=date("YmdHis", time());
+    $ip=getenv('REMOTE_ADDR')
+    
+    //$connect = mysql_connect('hostname','db_userid','pw');
+    $connect = mysql_connect('localhost','root','');//mysql connecting
+    //mysql_select_db('db_name','mysql_connect_name:$connect');
+    mysql_select_db('bbs_1', $connect);//selecting db
+    if(!$connect){
+        echo 'fail to cnnect to MySQL'.mysql_error();
+    }
+    //sending query
+    $query="insert into bbs_1(id, user_id, name, pw, memo, regdate, ip) 
+    values('$id','$user_id','$name','$pw','$memo','$regdate','$ip')";
+    mysql_query($query, $connect);
+
+    mysql_close;
+
+?>
+
+
+<script>
+    window.alert("query has been sent succesfully");
+    location.href="./list.php";
+</script>
