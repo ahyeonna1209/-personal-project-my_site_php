b9a50a3f3f2ddbb73474206a875f0c24bfd4a55a
ahyeonna1209 <36820261+ahyeonna1209@users.noreply.github.com>
2018-10-29 14:09:49 +1300

    Create create_table.php

---
+++ temp/create_table.php
@@ -0,0 +1,23 @@
+<?header
+    include("../lib/db_connect.php");
+    $connect=dbconn();
+    
+        //$sql="CREATE TABLE, table name,(field)";
+        //mysql_query();
+        
+        $sql="CREATE TALBE member
+        (no int not null auto_increment, 
+        PRIMARY KEY(no),
+        id char(15),
+        user_id char(15),
+        name char(15),
+        birth char (8),
+        sex char(6),
+        tel char(12),
+        email char(30),
+        pw char(30)
+        )";
+        mysql_query($sql, $connect);
+        if(!sql)die('fail to create table'.mysql_error());
+
+?>
